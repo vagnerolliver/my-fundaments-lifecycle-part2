b969ed6c00979412f1ea539500b62eab0ae297e1
Vágner Oliveira <vagner.oliveira@zenvia.com>
2020-12-06 11:57:29 -0300

    refactor project and system core

--- esmodule-internacionalization-02/src/index.js
+++ esmodule-internacionalization-02/src/index.js
@@ -1,36 +1,35 @@
-import DraftLog from 'draftlog'
-import chalk from 'chalk'
-import chalkTable from 'chalk-table'
-import readline from 'readline'
-
 import database from '../database.json'
 import Person from './person.js'
-
-DraftLog(console).addLineListener(process.stdin)
-
-const DEFAULT_LANG = 'pt-BR'
-const options = {
-    leftPad: 2,
-    columns: [ 
-        { field: "id", name: chalk.cyan("ID") },
-        { field: "vehicles", name: chalk.magenta("Vehicles") },
-        { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
-        { field: "from", name: chalk.magenta("From") },
-        { field: "to", name: chalk.cyan("To") }, 
-    ]
-}
-
-const table = chalkTable(options, database.map(item => new Person(item).formatted(DEFAULT_LANG)))
-const print = console.draft(table)
-
-const terminal = readline.createInterface({
-    input: process.stdin, 
-    output: process.stdout
-})
-
-terminal.question('Olá, diga o seu  nome! ', (msg)=> {
-    console.log('msg -->', msg.toString())
-})
+import TerminalController from './terminalController.js'
 
 
+ const DEFAULT_LANG = 'pt-BR'
+ const STOP_TERMINAL = ':q'
+
+ const terminalController = new TerminalController()
+ terminalController.initializeTerminial(database, DEFAULT_LANG)
+
+
+ async function mainLoop() {
+     try { 
+        const answer = await terminalController.question()
+        
+        if(answer === STOP_TERMINAL) { 
+            terminalController.closeTerminal()
+            return;
+        }
+
+        const person = Person.generateInstanceFromStrig(answer)
+        console.log('person', person.formatted(DEFAULT_LANG))
+
+        return mainLoop()
+     } 
+     catch (err) {
+         console.error('DEU RUIM**', err)
+         return mainLoop()
+     }
+ } 
+
+ await mainLoop()
+ 
 
--- esmodule-internacionalization-02/src/person.js
+++ esmodule-internacionalization-02/src/person.js
@@ -31,4 +31,18 @@ export default class Person {
                 .format(mapDate(this.to))
         }
     }
+ 
+    static generateInstanceFromStrig(text) {
+        const EMPTY_SPACE = ' ';
+        const [id, vehicles, kmTraveled, from, to] = text.split(EMPTY_SPACE);
+        const person = new Person({
+            id,
+            vehicles: vehicles.split(','), 
+            kmTraveled, 
+            from, 
+            to
+        });
+
+        return person
+    }
 }
